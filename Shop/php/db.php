97f551ed78799170ff3932003b0c7313b602bd10
LubovOst <163461938+LubovOst@users.noreply.github.com>
2024-04-21 15:20:02 +0300

    Push php

---
+++ Shop/php/db.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+$host = "localhost";
+$db = "ytvrbfvm_030";
+$user = "ytvrbfvm_030";
+$password = "123456L";
+
+$mysqli = mysqli_connect($host, $user, $password, $db);
